d1220c948afd99fd66a235bb5025782eb0632eee
Paolo Mainardi <paolomainardi@gmail.com>
2024-07-19 19:41:35 +0200

    feat: recalculate open pr using the same algorithm

--- evaluate.sh
+++ evaluate.sh
@@ -37,10 +37,10 @@ while IFS= read -r project; do
     # Get repository data
     REPO_DATA=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$OWNER/$REPO)
     STARS=$(echo $REPO_DATA | jq .stargazers_count)
-    COMMITS=$(curl -s -H "Authorization: token $GITHUB_TOKEN" --head https://api.github.com/repos/$OWNER/$REPO/commits\?sha\=main\&per_page\=1\&page\=1 | grep -i "Link:" | grep -o '<[^>]*>; rel="last"' | sed 's/.*<\(.*\)>.*/\1/' | sed 's/.*page=\([0-9]*\).*/\1/')
-    OPEN_PRS=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$OWNER/$REPO/pulls?state=open | jq length)
+    DEFAULT_BRANCH=$(echo $REPO_DATA | jq .default_branch)
+    COMMITS=$(curl -s -H "Authorization: token $GITHUB_TOKEN" --head https://api.github.com/repos/$OWNER/$REPO/commits\?sha\=${DEFAULT_BRANCH}\&per_page\=1\&page\=1 | grep -i "Link:" | grep -o '<[^>]*>; rel="last"' | sed 's/.*<\(.*\)>.*/\1/' | sed 's/.*page=\([0-9]*\).*/\1/')
+    OPEN_PRS=$(curl -s -H "Authorization: token $GITHUB_TOKEN" --head https://api.github.com/repos/$OWNER/$REPO/pulls?state=open\&per_page\=1\&page\=1 | grep -i "Link:" | grep -o '<[^>]*>; rel="last"' | sed 's/.*<\(.*\)>.*/\1/' | sed 's/.*page=\([0-9]*\).*/\1/')
     OPEN_ISSUES=$(echo $REPO_DATA | jq .open_issues)
-
 
     # New API call to fetch license information
     LICENSE_DATA=$(curl -s -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$OWNER/$REPO/license")
@@ -56,7 +56,7 @@ while IFS= read -r project; do
     SCORECARD_SCORE=$(scorecard --repo=https://github.com/$OWNER/$REPO --format=json --checks $SCORECARD_CHECKS | jq '.score')
 
     # Append each line of output to results.txt
-    echo -e "$project\t$STARS\t$COMMITS\t\t$OPEN_PRS\t$OPEN_ISSUES\t$SCORECARD_SCORE\t$LICENSE" >> results.txt
+    echo -e "$project\t$STARS\t$COMMITS\t$OPEN_PRS\t$OPEN_ISSUES\t$SCORECARD_SCORE\t$LICENSE" >> results.txt
 done < "$PROJECTS_FILE"
 
 column -t -s $'\t' results.txt
